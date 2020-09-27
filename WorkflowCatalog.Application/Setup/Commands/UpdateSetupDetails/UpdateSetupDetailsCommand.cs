7e22c979a932849e14d1a3eb3a456ed43d42e7a7
Can Gologlu <can.gologlu@gmail.com>
2020-09-27 21:43:55 +0300

    added updatesetupcommand

---
+++ WorkflowCatalog.Application/Setup/Commands/UpdateSetupDetails/UpdateSetupDetailsCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MediatR;
+using WorkflowCatalog.Domain.Enums;
+
+namespace WorkflowCatalog.Application.Setup.Commands.UpdateSetupDetails
+{
+    public class UpdateSetupDetailsCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public SetupStatus Status { get; set; }
+    }
+}
