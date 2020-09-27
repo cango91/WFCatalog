255eb77158aa764d1c4691bb8ec0633c74ec1348
Can Gologlu <can.gologlu@gmail.com>
2020-09-27 22:14:09 +0300

    * UpdateSetupDetailsCommand.cs: * UpdateSetupDetailsCommandValidator.cs:
    * NotFoundException.cs:

--- WorkflowCatalog.Application/Setup/Commands/UpdateSetupDetails/UpdateSetupDetailsCommand.cs
+++ WorkflowCatalog.Application/Setup/Commands/UpdateSetupDetails/UpdateSetupDetailsCommand.cs
@@ -1,13 +1,44 @@
-﻿using System;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using MediatR;
+using WorkflowCatalog.Application.Common.Exceptions;
+using WorkflowCatalog.Application.Common.Interfaces;
 using WorkflowCatalog.Domain.Enums;
 
 namespace WorkflowCatalog.Application.Setup.Commands.UpdateSetupDetails
 {
-    public class UpdateSetupDetailsCommand : IRequest<int>
+    public class UpdateSetupDetailsCommand : IRequest
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string ShortName { get; set; }
         public SetupStatus Status { get; set; }
+    }
+
+    public class UpdateSetupDetailsCommandHandler : IRequestHandler<UpdateSetupDetailsCommand>
+    {
+        private readonly IApplicationDbContext _context;
+        public UpdateSetupDetailsCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateSetupDetailsCommand request,CancellationToken cancellationToken)
+        {
+            var entity = await _context.Setups.FindAsync(request.Id);
+
+            if(entity == null)
+            {
+                throw new NotFoundException(nameof(WorkflowCatalog.Domain.Entities.Setup),request.Id);
+            }
+
+            entity.Name = request.Name;
+            entity.ShortName = request.ShortName;
+            entity.Status = request.Status;
+
+            return Unit.Value;
+
+        }
+
     }
 }
---
+++ WorkflowCatalog.Application/Setup/Commands/UpdateSetupDetails/UpdateSetupDetailsCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using WorkflowCatalog.Application.Common.Interfaces;
+
+namespace WorkflowCatalog.Application.Setup.Commands.UpdateSetupDetails
+{
+    public class UpdateSetupDetailsCommandValidator : AbstractValidator<UpdateSetupDetailsCommand>
+    {
+        private readonly IApplicationDbContext _context;
+        public UpdateSetupDetailsCommandValidator(IApplicationDbContext context)
+        {
+            _context = context;
+
+
+        }
+
+        public async Task<bool> BeUniqueAbbreviation(UpdateSetupDetailsCommand model, string abb,CancellationToken cancellationToken)
+        {
+            return await _context.Setups
+                .Where(x => x.Id != model.Id)
+                .AllAsync(x => !String.Equals(x.ShortName.ToLowerInvariant(),abb.ToLowerInvariant()));
+
+        }
+
+    }
+}
