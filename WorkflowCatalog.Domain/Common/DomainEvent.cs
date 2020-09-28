a830d2fc2a2021de16a6053d409735145c49420d
Can Gologlu <can.gologlu@gmail.com>
2020-09-28 21:31:53 +0300

    Added missing DomainEvent file

---
+++ WorkflowCatalog.Domain/Common/DomainEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkflowCatalog.Domain.Common
+{
+    public interface IHasDomainEvent
+    {
+        public List<DomainEvent> DomainEvents { get; set; }
+    }
+
+
+    public abstract class DomainEvent
+    {
+        protected DomainEvent()
+        {
+            DateOccured = DateTimeOffset.UtcNow;
+        }
+        public DateTimeOffset DateOccured { get; protected set; } = DateTime.UtcNow;
+    }
+}
