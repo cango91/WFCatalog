425d5aba2e4b81b56c5b004d8264294d57623354
Mücahit Tekin <mucahit.tekin@acibadem.com>
2020-11-01 17:25:01 +0300

    Fixed conflicts

--- WorkflowCatalog.API/Startup.cs
+++ WorkflowCatalog.API/Startup.cs
@@ -14,6 +14,7 @@ using WorkflowCatalog.API.Identity;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Sieve.Services;
 using Sieve.Models;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 
 namespace WorkflowCatalog.API
 {
@@ -46,7 +47,22 @@ namespace WorkflowCatalog.API
 
             services.Configure<SieveOptions>(Configuration.GetSection("Sieve"));
 
-            
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+
+            services.AddOpenApiDocument(configure =>
+            {
+                configure.Title = "SadaPayBackend API";
+                configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Name = "Authorization",
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Description = "Type into the textbox: Bearer {your JWT token}."
+                });
 
 
 
@@ -81,6 +97,20 @@ namespace WorkflowCatalog.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    //spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                }
             });
         }
     }
