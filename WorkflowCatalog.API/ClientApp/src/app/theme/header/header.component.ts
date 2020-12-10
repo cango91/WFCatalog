006b9c796b281c9376bf781167a6760b5a51e972
Can Gol <can.gologlu@gmail.com>
2020-12-10 04:19:44 +0300

    added logout redirection

--- WorkflowCatalog.API/ClientApp/src/app/theme/header/header.component.ts
+++ WorkflowCatalog.API/ClientApp/src/app/theme/header/header.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NbMenuService } from '@nebular/theme';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit,OnDestroy {
 
   items = [
-    { title: 'Settings' },
-    { title: 'Logout' },
+    { title: 'Settings', link: environment.userSettingsUrl},
+    { title: 'Logout', link: environment.logoutUrl},
   ];
 
-  constructor() { }
+  subscription: Subscription
+
+  constructor(protected menuService: NbMenuService) { }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
+    this.subscription = this.menuService.onItemClick()
+      .pipe(
+        filter(({tag}) => tag === 'user-menu')
+      )
+      .subscribe(data => {
+        window.location.href = data.item.link;
+        });
   }
 
 }
--- WorkflowCatalog.API/ClientApp/src/environments/environment.prod.ts
+++ WorkflowCatalog.API/ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   loginUrl: 'http://catalog.acibadem.com.tr/ProductCatalog/Account/Login',
+  logoutUrl: 'http://catalog.acibadem.com.tr/ProductCatalog/Account/Logout',
+  userSettingsUrl: '#',
 };
